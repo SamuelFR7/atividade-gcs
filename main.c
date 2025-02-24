171f92a5871cbd47eb37340268859a82fcc8419e
Samuel Ferreira <samuel.ferreira@ipeweb.com.br>
2025-02-24 19:25:22 -0300

    versão quatro: refatorando código em funções

--- main.c
+++ main.c
@@ -1,21 +1,33 @@
 #include <stdio.h>
 
+char *captureName(char **argv);
+char *captureLastName(char **argv);
+
 int main(int argc, char **argv)
 {
-  char *name = "world";
-
-  char *nameArg = argv[1];
-  char *lastName = argv[2]; 
-
-  if (nameArg != NULL) {
-    name = nameArg;
-  }
+  char *name = captureName(argv);
+  char *lastName = captureLastName(argv);
   
-  if (lastName) {
+  if (lastName != NULL) {
     printf("Hello %s %s!\n", name, lastName);
   } else {
     printf("Hello %s!\n", name);
   }
 
   return 0;
+}
+
+char *captureName(char **argv) {
+  if (argv[1] != NULL) {
+    return argv[1];
+  }
+
+  return "world";
+}
+
+char *captureLastName(char **argv) {
+  if (argv[2] != NULL) {
+    return argv[2];
+  }
+  return NULL;
 }
