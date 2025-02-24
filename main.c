c07e123251ed536cfb6661c37fa535246cf66205
Samuel Ferreira <samuel.ferreira@ipeweb.com.br>
2025-02-24 19:21:17 -0300

    versão três: aceitando sobrenome

--- main.c
+++ main.c
@@ -5,12 +5,17 @@ int main(int argc, char **argv)
   char *name = "world";
 
   char *nameArg = argv[1];
+  char *lastName = argv[2]; 
 
   if (nameArg != NULL) {
     name = nameArg;
   }
-
-  printf("Hello %s!\n", name);
+  
+  if (lastName) {
+    printf("Hello %s %s!\n", name, lastName);
+  } else {
+    printf("Hello %s!\n", name);
+  }
 
   return 0;
 }
