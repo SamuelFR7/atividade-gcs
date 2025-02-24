18dbafab0dd8aca0c74d64821fc2058a4c16d7f8
Samuel Ferreira <samuel.ferreira@ipeweb.com.br>
2025-02-24 19:17:55 -0300

    versão dois aceitando argumento

--- main.c
+++ main.c
@@ -1,8 +1,16 @@
 #include <stdio.h>
 
-int main() 
+int main(int argc, char **argv)
 {
-  printf("Hello world!");
+  char *name = "world";
+
+  char *nameArg = argv[1];
+
+  if (nameArg != NULL) {
+    name = nameArg;
+  }
+
+  printf("Hello %s!\n", name);
 
   return 0;
 }
